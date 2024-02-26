b20cc18a6c83c7dd6ccc6c87060de534797016dd
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-02-26 14:27:43 -0300

    Fix deploy images error

--- src/components/Knowledge/data.ts
+++ src/components/Knowledge/data.ts
@@ -1,54 +1,54 @@
 export const dataKnowledge = [
     {
         "title": "React",
-        "icon": "../public/icons/react.svg"
+        "icon": "react.svg"
     },
     {
         "title": "TypeScript",
-        "icon": "../public/icons/typescript.svg"
+        "icon": "typescript.svg"
     },
     {
         "title": "JavaScript",
-        "icon": "../public/icons/js.svg"
+        "icon": "js.svg"
     },
     {
         "title": "HTML5",
-        "icon": "../public/icons/html5.svg"
+        "icon": "html5.svg"
     },
     {
         "title": "CSS3",
-        "icon": "../public/icons/css3.svg"
+        "icon": "css3.svg"
     },
     {
         "title": "SASS",
-        "icon": "../public/icons/sass.svg"
+        "icon": "sass.svg"
     },
     {
         "title": "Tailwind",
-        "icon": "../public/icons/tailwind.svg"
+        "icon": "tailwind.svg"
     },
     {
         "title": "Bootstrap",
-        "icon": "../public/icons/Bootstrap.svg"
+        "icon": "Bootstrap.svg"
     },
     {
         "title": "GIT",
-        "icon": "../public/icons/git.svg"
+        "icon": "git.svg"
     },
     {
         "title": "SQL",
-        "icon": "../public/icons/sql.svg"
+        "icon": "sql.svg"
     },
     {
         "title": "C#",
-        "icon": "../public/icons/csharp.svg"
+        "icon": "csharp.svg"
     },
     {
         "title": "Node",
-        "icon": "../public/icons/node.svg"
+        "icon": "node.svg"
     },
     {
         "title": "Styled-Components",
-        "icon": "../public/icons/styled-components.svg"
+        "icon": "styled-components.svg"
     }
 ]
--- src/components/MyProjects/dataProjects.ts
+++ src/components/MyProjects/dataProjects.ts
@@ -3,7 +3,7 @@ export const dataProjects = [
         "id": 1,
         "title": "E-Commerce TCC",
         "description": "O projeto Ecommerce TCC foi desenvolvido utilizando as tecnologias React, TypeScript e estilização através de módulos SASS. Seu principal propósito foi consolidar meus conhecimentos para apresentação aos professores da faculdade. Além do design, um aspecto valioso do site é a componentização dos elementos gráficos, a modularização dos estilos, a tipagem de dados e a organização e legibilidade do código.",
-        "img": "../public/projects/tcc.png",
+        "img": "tcc.png",
         "techs": 
             [
                 "React",
@@ -18,7 +18,7 @@ export const dataProjects = [
         "id": 2,
         "title": "Organo",
         "description": "O Projeto Organo foi desenvolvido utilizando React, com o intuito principal de aprimorar os meus estudos e conhecer a biblioteca. O site gera cartões individuais de perfil para membros de uma equipe específica. Os usuários podem inserir seus nomes, cargos, imagens de perfil e time de afiliação. Além disso, os usuários têm a opção de criar equipes personalizadas, especificando nomes e cores para as equipes. Esse foi o meu primeiro projeto desenvolvido com React, nele aprendi como os componentes funcionam, como passar props entre eles, a usar o useState e a como funciona a mudança dos estados, usei também o uuid, para gerar códigos únicos para identificar o nome dos times",
-        "img": "../public/projects/organo.png",
+        "img": "organo.png",
         "techs": 
             [
                 "React",
@@ -33,7 +33,7 @@ export const dataProjects = [
         "id": 3,
         "title": "Hello World",
         "description": "O projeto Hello Word foi criado empregando as tecnologias React e estilização por meio do CSS. Seu propósito central consistiu em aprofundar minha compreensão da biblioteca, especialmente no que se refere à implementação de rotas com o React-Router-Dom. O site apresenta 3 opções de rotas no Header: O 'Início' contém artigos que abordam tecnologias relacionadas ao campo do desenvolvimento. Quando um desses artigos é clicado, o usuário é redirecionado para páginas cujo URL é modificado para incluir o número de identificação único de cada artigo.O 'Sobre Mim', onde descrevo um pouco da minha pessoa, da minha trajetória e curiosidades sobre mim; E o 'Teste', onde é somente uma tela mostrando o que aconteceria se o usuário digitasse no URL uma rota que não existe.",
-        "img": "../public/projects/hello-world.png",
+        "img": "hello-world.png",
         "techs": 
             [
                 "React",
@@ -46,7 +46,7 @@ export const dataProjects = [
         "id": 4,
         "title": "Space",
         "description": "O Projeto Alura Space foi elaborado utilizando as tecnologias React e SASS, com o objetivo principal de aprimorar minha compreensão da criação de componentes e da utilização de estilos externos. Em essência, o site apresenta cartões que exibem imagens de galáxias, estrelas, luas e nebulosas. É possível realizar filtragens com base em opções específicas e restaurar a visualização de todas as imagens. Todas as informações são dinâmicas, uma vez que os títulos, créditos, URLs das imagens, IDs e tags para filtragem são fornecidos por meio de um arquivo JSON.",
-        "img": "../public/projects/space.png",
+        "img": "space.png",
         "techs": 
             [
                 "React",
@@ -59,7 +59,7 @@ export const dataProjects = [
         "id": 5,
         "title": "Newsletter",
         "description": "O projeto do Newsletter foi desenvolvido usando HTML e Tailwind. Possui a principal funcionalde de estudar e testar a tecnologia Tailwind. A pagina possui responsividade para 3 tipos de telas diferentes: Desktop, Laptop e Mobile. Possui também animações no botão 'Inscreva-se' e no sino, que faz um shake e sinaliza que está emitindo som.",
-        "img": "../public/projects/newsletter.png",
+        "img": "newsletter.png",
         "techs":
         [
             "HTML",
