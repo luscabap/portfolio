71b98d5305271606a7310ce9eca53983492cc22b
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-06-25 19:09:50 -0300

    feat: configurating i18n

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import HomePage from './Pages/HomePage';
+import "./lib/i18n";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
