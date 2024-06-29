890e7cb3d0fff2d101a828574aa3027fbfdd5720
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-06-29 16:59:12 -0300

    fix: removing stories test

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,38 @@
 import type { Preview } from "@storybook/react";
+import '../src/styles/global';
+import '../src/styles/styled';
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import lightTheme from '../src/styles/themes/light';
+import darkTheme from '../src/styles/themes/dark';
+import { Decorator } from '@storybook/react';
+import GlobalStyle from '../src/styles/global';
+
+const withThemeProvider = (Story, context) => {
+  const theme = context.globals.theme === 'dark' ? darkTheme : lightTheme;
+
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Story {...context} />
+    </ThemeProvider>
+  );
+};
+
+Decorator(withThemeProvider);
+
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Global theme for components',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'circlehollow',
+      items: ['light', 'dark'],
+      showName: true,
+    },
+  },
+};
 
 const preview: Preview = {
   parameters: {
---
+++ src/components/Contact/Contact.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta } from "@storybook/react";
+import Conctact from "./Contact";
+
+const meta: Meta<typeof Conctact > = {
+  component: Conctact
+};
+
+export default {
+  title: "Components/Contact",
+  component: Conctact
+};
+
+export const Default = {}
