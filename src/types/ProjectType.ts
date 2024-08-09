3b5e031ac2e82c46eb8b2d28b76cd99ba0e4826a
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-08-08 23:18:51 -0300

    project type

---
+++ src/types/ProjectType.ts
@@ -0,0 +1,12 @@
+export type ProjectProps = {
+  title: string,
+  description: string,
+  subtitulo: string,
+  img: string,
+  techs: string[],
+  deploy: string,
+  repo: string,
+  repoName: string,
+  concluido: string,
+  primario?: string,
+}
