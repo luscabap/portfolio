26d78ff4f43aff7918f38b03006f32f26ae5eff6
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-08-12 19:02:51 -0300

    feat: base component ModalSucessEmail

---
+++ src/components/ModalSuccessEmail/ModalSuccessEmail.tsx
@@ -0,0 +1,11 @@
+import * as Styles from "./styles";
+
+export const ModalSuccessEmail = () => {
+  return (
+    <Styles.ContainerBackgroundModal>
+      <Styles.ContainerModalSuccessEmail>
+        <h4>Success</h4>
+      </Styles.ContainerModalSuccessEmail>
+    </Styles.ContainerBackgroundModal>
+  )
+}
---
+++ src/components/ModalSuccessEmail/styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ContainerModalSuccessEmail = styled.div`
+  background-color: gray;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const ContainerBackgroundModal = styled.div`
+  background-color: rgba(0, 0, 0, 0.8);
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+`
